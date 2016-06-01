f8b3023e586d5d59321f08c5ff7b8e74c12f2d5c
pschang <pschang@ucsd.edu>
2016-06-01 07:49:33 -0700

    everything

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,9 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -24,6 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         }
+
+    public void click(View view)
+    {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("bye world!");
+    }
+
+    public void clickIntent(View view)
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
 
 
 
--- app/src/test/java/com/example/myapplication/RobolectricTest.java
+++ app/src/test/java/com/example/myapplication/RobolectricTest.java
@@ -1,30 +1,59 @@
 package com.example.myapplication;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.ComponentName;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.robolectric.Shadows.shadowOf;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class RobolectricTest {
     @Test
     public void testFirst() {
+        Activity activity = Robolectric.setupActivity(MainActivity.class);
+        TextView results = (TextView) activity.findViewById(R.id.textView);
+        String resultsText = results.getText().toString();
+
+        // failing test gives much better feedback to show that all works
+        // correctly
+        assertThat(resultsText, equalTo("Hello world!"));
 
     }
     @Test
     public void testSecond()
     {
+        Activity activity = Robolectric.setupActivity(MainActivity.class);
+        TextView results = (TextView) activity.findViewById(R.id.textView);
+        Button but = (Button) activity.findViewById(R.id.button);
+
+        but.performClick();
+        String resultsText = results.getText().toString();
+        assertThat(resultsText, equalTo("bye world!"));
+    }
+
+    @Test
+    public void testIntent()
+    {
+        Activity activity = Robolectric.setupActivity(MainActivity.class);
+        ShadowActivity shadowActivity = Shadows.shadowOf(activity);
+        Button button = (Button) activity.findViewById(R.id.button2);
+        button.performClick();
+        // The intent we expect to be launched when a user clicks on the button
+        assertThat(shadowActivity
+                .peekNextStartedActivityForResult()
+                .intent.getComponent(), equalTo(new ComponentName(activity, Main2Activity.class)));
+
 
     }
 
